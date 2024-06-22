a695955c64bf8de77c3d48c47b9ba5e07944cafb
rehankhan06 <153363380+rehankhan06@users.noreply.github.com>
2024-06-22 06:15:34 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144223 
+let accountEmail = "rehankhan@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ahemdabad"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "654321"
+accountCity = "Bangloru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity,])
